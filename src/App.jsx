fe8969ef0b3b655d8a8c238ac25a6b14bff3ae5f
Rohit <rk.rohitagrawal@gmail.com>
2023-03-04 18:52:45 +0530

    added intersection events

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,78 @@ import styles from "./App.module.css";
 import Partners from "./components/Partners/Partners";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import ReactPixel from 'react-facebook-pixel';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const advancedMatching = { em: 'sachinsinghjagawat@gmail.com' }; // optional, more info: https://developers.facebook.com/docs/facebook-pixel/advanced/advanced-matching
 const options = {
   autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
   debug: true, // enable logs
 };
-ReactPixel.init('1233452067589283', advancedMatching, options);
+ReactPixel.init('1233452067589283', "", options);
 function App() {
-
-  ReactPixel.pageView(); // For tracking page view
-  ReactPixel.trackCustom("LeadGenerated", {value: "1", currency: "USD" });
+  const [isIntersecting, setIsIntersecting] = useState({
+    landing: false,
+    whyUs: false,
+    ideas: false,
+    servicesProvided: false,
+    growth: false,
+    projectsDone: false,
+    howWeDevelop: false,
+    team: false,
+    testimonials: false,
+    faqs: false,
+  });
+  useEffect(()=>{
+    ReactPixel.pageView(); // For tracking page view
+  }, [])
+  useEffect(()=>{
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting){
+          if (entry.target.className === "landing" && !isIntersecting.landing) {
+          ReactPixel.trackCustom('LandingPageView', {value: 1, currency: 'USD'});
+          setIsIntersecting((state)=>{
+            return {...state, landing: true}
+          });
+        }
+        if (entry.target.className === "projectsDone" && !isIntersecting.projectsDone) {
+          console.log("intersecting projects done");
+          ReactPixel.trackCustom('ProjectsDonePageView', {value: 2, currency: 'USD'});
+          setIsIntersecting((state)=>{
+            return {...state, projectsDone: true}
+          });
+        }
+        if (entry.target.className === "servicesProvided" && !isIntersecting.servicesProvided) {
+          console.log("intersecting services provided");
+          ReactPixel.trackCustom('ServicesProvidedPageView', {value: 3, currency: 'USD'});
+          setIsIntersecting((state)=>{
+            return {...state, servicesProvided: true}
+          });
+        }
+        if (entry.target.className === "testimonials" && !isIntersecting.testimonials) {
+          console.log("intersecting testimonials");
+          ReactPixel.trackCustom('TestimonialsPageView', {value: 4, currency: 'USD'});
+          setIsIntersecting((state)=>{
+            return {...state, testimonials: true}
+          });
+        }
+      }
+      });
+    });
+    const landingElement = document.querySelector(".landing");
+    const projectsDoneElement = document.querySelector(".projectsDone");
+    const servicesProvidedElement = document.querySelector(".servicesProvided");
+    const testimonialsElement = document.querySelector(".testimonials");
+    observer.observe(landingElement);
+    observer.observe(projectsDoneElement);
+    observer.observe(servicesProvidedElement);
+    observer.observe(testimonialsElement);
+    return () => {
+      observer.unobserve(landingElement);
+      observer.unobserve(projectsDoneElement);
+      observer.unobserve(servicesProvidedElement);
+      observer.unobserve(testimonialsElement);
+    }
+  },[isIntersecting])
   const analytics = getAnalytics();
   console.log(
     logEvent(analytics, "select_content", {
@@ -36,7 +96,8 @@ function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Landing />
+      <div className="landing"><Landing /></div>
+      
       <div className={styles.circleBg}>
         <div className={styles.greyMountainWrapper}>
           <WhyUs />
@@ -44,12 +105,13 @@ function App() {
         <div className={styles.lineBgWrapper}>
           <Ideas />
         </div>
-        <ServicesProvided />
+        <div className="servicesProvided"><ServicesProvided /></div>
+        
         <div className={styles.lineBgWrapper}>
           <Growth />
         </div>
         <div className={styles.greyMountainWrapper}>
-          <ProjectsDone />
+          <div className="projectsDone"><ProjectsDone /></div>   
         </div>
         <div className={styles.lineBgWrapper}>
           <HowWeDevelop />
@@ -58,7 +120,7 @@ function App() {
         <Partners />
 
         <div className={styles.lineBgWrapper}>
-          <Testimonials />
+          <div className="testimonials"><Testimonials /></div>
         </div>
         <FAQs />
         <Footer />
