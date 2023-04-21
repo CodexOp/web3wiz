52b15a6b257a7c5035a53ac20a2b6c8d0175eed6
Aman Singh Chauhan <amansingh9723chauhan@gmail.com>
2023-04-21 18:07:25 +0530

    ui update

--- src/pages/Newsletter/Newsletter.jsx
+++ src/pages/Newsletter/Newsletter.jsx
@@ -6,19 +6,21 @@ import { ReactComponent as Logo } from './assets/logo.svg';
 const Newsletter = () => {
   return (
     <>
-      <div className={styles.container}>
-        <Logo className={styles.logo} />
-        <div className={styles.box}>
-          <p>
-            Don’t miss updates on <span>important events</span>
-          </p>
-          <div>
-            <input type="email" placeholder="email address" />
-            <button> Sign Up</button>
+      <div className={styles.newsletter}>
+        <div className={styles.container}>
+          <Logo className={styles.logo} />
+          <div className={styles.box}>
+            <p>
+              Don’t miss updates on <span>important events</span>
+            </p>
+            <div>
+              <input type="email" placeholder="email address" />
+              <button> Sign Up</button>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
